8c219d98dd6e4738893dc8f667d06373f2bdcc51
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-04-03 14:22:17 +0700

    Add project files.

---
+++ ActivityStructure_201/ActivityStructure_201.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Alamat {
+    char desa;
+    char kota;
+};
+
+struct mahasiswa {
+    string nim;
+    string nama;
+    string alamat;
+    int umur;
+};
+
+int main()
+{
+    Mahasiswa mhs1, mhs2;
+
+    mhs1.nim = 20220140201;
+}
+
