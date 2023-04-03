d76e2245922a08098d30543303b144494df8761d
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-04-03 14:57:44 +0700

    Input struck Mahasiswa dengan alamat lengkap melalui console

--- Structure2_201/Structure2_201.cpp
+++ Structure2_201/Structure2_201.cpp
@@ -13,3 +13,20 @@ struct mahasiswa {
     AlamatDetail alamat;
     int umur;
 };
+
+int main()
+{
+    mahasiswa mhs;
+
+    cout << "Masukkan Nim :";
+    cin >> mhs.nim;
+    cout << "Masukkan Nama :";
+    cin >> mhs.nama;
+    cout << "Alamat :" << endl;
+    cout << "\tDesa :";
+    cin >> mhs.alamat.desa;
+    cout << "\tKota :";
+    cin >> mhs.alamat.kota;
+    cout << "Masukkan umur :";
+    cin >> mhs.umur;
+}
